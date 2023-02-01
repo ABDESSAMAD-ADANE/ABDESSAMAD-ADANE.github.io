fa89cf9cf57a2dfe6ed4ee4735c99994bf879ce2
adaneabdessamad <adaneabdessamad@gmailcom>
2023-02-01 21:26:35 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  expression = '';
+
+  addNumber(number: string) {
+    this.expression += number;
+  }
+
+  addOperator(operator: string) {
+    this.expression += operator;
+  }
+
+  calculate() {
+    this.expression = eval(this.expression);
+  }
 }
